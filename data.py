5c13f9dad28322debc160a83f1bcbb4a84d84e80
JeonSeongjoon <02seongjoom@gmail.com>
2025-09-18 17:50:21 +0900

    pre

--- data.py
+++ data.py
@@ -15,10 +15,12 @@ def Logging(output, save_path):
     if not os.path.exists(save_path):
         os.mkdir(save_path)
 
-    res_path = os.path.join(save_path, 'Output.xlsx')
+    res_path = os.path.join(save_path, 'output.xlsx')
 
     res = pd.DataFrame(output)
     res.to_excel(res_path)
 
 
-# {"paragraphs" : , "problems" :, "answers" :, "parag_num" : , "prob_num" : },
+
+# The dict below is a format of problem sample.
+# {"paragraphs" : , "problems" :, "answers" :, "parag_num" : , "prob_num" : }
